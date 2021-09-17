b3afa0573bf9a37d012ef13142fa15ea598b319d
Pietro Di Bello <pierodibello@gmail.com>
2021-09-17 21:44:58 +0200

    r extract method to simplify pipeline

--- src/main/java/com/kata/bankocr/BankOCR.java
+++ src/main/java/com/kata/bankocr/BankOCR.java
@@ -21,10 +21,12 @@ public class BankOCR {
             return emptyList();
 
         return IntStream.iterate(0, i -> i < lines.size(), i -> i + 4)
-                .mapToObj(i -> {
-                    AccountNumberRow accountNumberRow = new AccountNumberRow(lines.get(i), lines.get(i + 1), lines.get(i + 2));
-                    return accountNumberParser.parse(accountNumberRow);
-                })
+                .mapToObj(i -> toAccountNumber(lines, i))
                 .collect(toList());
+    }
+
+    private String toAccountNumber(List<String> lines, int i) {
+        AccountNumberRow accountNumberRow = new AccountNumberRow(lines.get(i), lines.get(i + 1), lines.get(i + 2));
+        return accountNumberParser.parse(accountNumberRow);
     }
 }
